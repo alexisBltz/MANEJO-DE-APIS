2ed5ca0fefe1cecaf8f1b8e33cb7e329623cf484
alexisBltz <132708738+alexisBltz@users.noreply.github.com>
2023-12-30 09:58:22 -0500

    Añadiendo avance a mi repositorio: creanding metodo para obtener los topics y otro def para sacar los codigos del drive del modelo

--- classroom.py
+++ classroom.py
@@ -8,24 +8,58 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/classroom.courses.readonly",
-          "https://www.googleapis.com/auth/drive.metadata.readonly"]
+          "https://www.googleapis.com/auth/drive.metadata.readonly",
+          "https://www.googleapis.com/auth/classroom.courseworkmaterials"]
 
 
 def main():
   creds = otorgarPermisos()
-  classroom = mostrarClassroom(creds)
-  print(classroom)
-  drive(creds)
+  idClassrooms = mostrarClassroom(creds)
+  print(idClassrooms)
+  mi_clase , clase_modelo = idClassrooms
+  sacarlinksSemana(creds, idClassrooms[clase_modelo])
+  #drive(creds)
 
+def obtener_id_tema_por_nombre(creds, course_id_modelo, nombre_tema):
+    service = build('classroom', 'v1', credentials=creds)
+
+    # Obtener la lista de temas del curso
+    lista_temas = service.courses().topics().list(courseId=course_id_modelo).execute()
+
+    for tema in lista_temas.get('topic', []):
+        if tema['name'] == nombre_tema:
+            return tema['topicId']
+
+    # Devolver None si no se encuentra el tema
+    return None
+
+def sacarlinksSemana(creds , course_id_modelo, topic):
+    service = build('classroom', 'v1', credentials=creds)
+
+    listaMateriales = service.courses().courseWorkMaterials().list(courseId=course_id_modelo).execute()
+    print(listaMateriales)
+    for material in listaMateriales['courseWorkMaterial']:
+            for material_info in material['materials']:
+                if 'driveFile' in material_info:
+                    drive_file_info = material_info['driveFile']['driveFile']
+
+                    drive_file_id = drive_file_info['id']
+                    drive_file_title = drive_file_info['title']
+                    print(f"NOMBRE: {drive_file_title}")
+                    print(f"ID DRIVE: {drive_file_id}")
+
+                else:
+                    print("No hay información de Google Drive en este material de trabajo.")
 
 def drive(creds):
+    folder_id = 'MODELOS DE PARTE'
     try:
         service = build("drive", "v3", credentials=creds)
 
         # Call the Drive v3 API
         results = (
             service.files()
-            .list(pageSize=10, fields="nextPageToken, files(id, name)")
+            .list(q=f"'{folder_id}' in parents", pageSize=10, fields="nextPageToken, files(id, name)")
             .execute()
         )
         items = results.get("files", [])
